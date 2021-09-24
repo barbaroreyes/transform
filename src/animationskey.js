6168752fa584c733420b6792efe5d308cdbb6e43
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-24 09:12:57 -0500

    udateanimation

---
+++ src/animationskey.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const animationskey = () => {
+  return (
+    <div>
+      <div className="key"><h1>Keyframe-rotate</h1></div>
+      
+    </div>
+  )
+}
+
+export default animationskey
